049586203423f5fa706ec9829ed05133ef54cd69
PRIYAM PATEL <patelpriyam203@gmail.com>
2018-01-15 00:41:14 -0500

    View Active Tasks Completed

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,21 @@
 (function() {
-  function HomeCtrl(Task) {
+  function HomeCtrl(Task, $scope) {
     this.tasks = Task.all;
+
+    this.addTask = function(taskTitle, taskDescription) {
+      if (taskTitle) {
+        var newTask = {
+          title: taskTitle,
+          description: taskDescription,
+          createdAt: Date.now(),
+          completed: false
+        };
+        Task.addTask(newTask);
+      }
+    }
   }
 
   angular
     .module('Blocitoff')
-    .controller('HomeCtrl', ['Task', HomeCtrl]);
+    .controller('HomeCtrl', ['Task', '$scope', HomeCtrl]);
 })();
--- app/scripts/services/Task.js
+++ app/scripts/services/Task.js
@@ -1,19 +1,19 @@
 (function() {
   function Task($firebaseArray) {
-    var Task = {};
+    // var Task = {};
     var ref = firebase.database().ref().child("tasks");
     var tasks = $firebaseArray(ref);
 
     Task.all = tasks;
 
-    Task.add = function(newTask) {
-      rooms.$add(newTask);
+    Task.addTask = function(newTask) {
+      tasks.$add(newTask);
     }
 
     return Task;
   }
 
   angular
-    .module('blocitoff')
+    .module('Blocitoff')
     .factory('Task', ['$firebaseArray', Task]);
 })();
